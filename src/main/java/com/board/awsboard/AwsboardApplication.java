c97085b0c1314a19f050467c192fea84b1c1afe9
chb <gyqls234@gmail.com>
2022-10-11 17:59:55 +0900

    first commit

---
+++ src/main/java/com/board/awsboard/AwsboardApplication.java
@@ -0,0 +1,13 @@
+package com.board.awsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsboardApplication.class, args);
+	}
+
+}
