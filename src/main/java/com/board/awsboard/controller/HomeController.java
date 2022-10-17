e678d35ac382f3419fedea9ecc63255fa62ab46d
chb <gyqls234@gmail.com>
2022-10-17 11:16:51 +0900

    [1017] thymeleaf exercise

--- src/main/java/com/board/awsboard/controller/HomeController.java
+++ src/main/java/com/board/awsboard/controller/HomeController.java
@@ -1,18 +1,16 @@
 package com.board.awsboard.controller;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class HomeController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping
+    public String index() {
+        return "index";
     }
 
     @GetMapping("/main")
