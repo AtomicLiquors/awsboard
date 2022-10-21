983931d73597c754ffd1e35c09ef61a9a1af9262
chb <gyqls234@gmail.com>
2022-10-21 17:02:05 +0900

    [1021] validation

--- src/main/java/com/board/awsboard/model/Board.java
+++ src/main/java/com/board/awsboard/model/Board.java
@@ -1,11 +1,14 @@
 package com.board.awsboard.model;
 
 import lombok.Data;
+import org.hibernate.annotations.NotFound;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -13,6 +16,9 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull
+    @Size(min=2, max=30)
     private String title;
     private String content;
 }
